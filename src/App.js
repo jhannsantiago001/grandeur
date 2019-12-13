428ebceca4dab9a0de37f3e5437bad582fb538ae
Jhann Santiago <jhann.santiago001@gmail.com>
2019-12-12 22:17:36 -0800

    add custom button component and style signin form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
+
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -16,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path = '/' component = {HomePage}/>
         <Route path = '/shop' component = {ShopPage}/>
+        <Route path = '/signin' component = {SignInAndSignUpPage}/>
       </Switch>
       </div>
   );
